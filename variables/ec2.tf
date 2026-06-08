f2d037f87fcea0d0b05fbad95a69ab24e46afc32
niky <enugandula.niranjan@gmail.com>
2026-06-08 22:12:09 +0530

    added tf ec2 config

---
+++ variables/ec2.tf
@@ -0,0 +1,29 @@
+resource "aws_instance" "this" {
+  ami                    = var.ami_id # This is our devops-practice AMI ID
+  vpc_security_group_ids = [aws_security_group.allow_tls.id]
+  instance_type          = var.instance_type
+  tags = var.ec2_tag
+}
+
+resource "aws_security_group" "allow_tls" {
+  name        = "allow_tls_1"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "allow_tls"
+  }
+}
