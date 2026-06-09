c66e9419c3eae5c7b5f7f335a94ae20139d5c7f2
niranjan <nikyniranjan31@gmail.com>
2026-06-09 13:55:12 +0530

    add

---
+++ datasources/data.tf
@@ -0,0 +1,24 @@
+data "aws_ami" "expense" {
+  most_recent      = true
+  owners           = ["973714476881"]
+
+  filter {
+    name   = "name"
+    values = ["Redhat-9-DevOps-Practice"]
+  }
+
+  filter {
+    name   = "root-device-type"
+    values = ["ebs"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
+
+
+output "ami_id" {
+    value = data.aws_ami.expense.id
+}
